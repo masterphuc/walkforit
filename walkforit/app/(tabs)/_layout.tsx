d6ab27d04f79d144bfbcd6f859fe44c2d4d0ad50
phucboi <phuc.nguyen123456@gmail.com>
2025-03-30 10:28:03 -0400

    Expo tutorial through part 4

--- walkforit/app/(tabs)/_layout.tsx
+++ walkforit/app/(tabs)/_layout.tsx
@@ -1,45 +1,33 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
+import { Tabs } from "expo-router";
+import {Ionicons} from "@expo/vector-icons";
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+export default function TabsLayout()
+{
+    return (
+        <Tabs
+            screenOptions={{
+                tabBarActiveTintColor: "red",
+                headerStyle: {backgroundColor: "#25292e"},
+                headerShadowVisible: false,
+                headerTintColor: "white",
+                tabBarStyle: {backgroundColor: "#25292e"},
+            }}>
+            <Tabs.Screen name="index"
+                          options={{headerTitle: "Sticker SMash",
+                              tabBarIcon: ({focused, color}) =>
+                                  <Ionicons name={focused ? "home-sharp" : "home-outline"}
+                                            color={color}
+                                            size={30} />,
+                              headerLeft: () => <></>}}/>
+            <Tabs.Screen name="about" options={{
+                                            headerTitle: "About Mash",
+                                            tabBarIcon: ({focused, color}) =>
+                                                <Ionicons
+                                                    name={focused ? 'information-circle' : 'information-circle-outline'}
+                                                    color={color}
+                                                    size={30} />,
+                                            }}/>
+            <Tabs.Screen name="not-found" options={{headerShown: false}} />
+        </Tabs>
+    );
 }
